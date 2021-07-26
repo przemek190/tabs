b1d7dad39c493b17d83e70bf65dc6094d71cf4c2
unknown <przemeklachut3bi@gmail.com>
2021-07-26 15:23:08 +0200

    1.0v

--- src/App.js
+++ src/App.js
@@ -2,8 +2,71 @@ import React, { useState, useEffect } from 'react'
 import { FaAngleDoubleRight } from 'react-icons/fa'
 
 const url = 'https://course-api.com/react-tabs-project'
+
 function App() {
-  return <h2>tabs project setup</h2>
+  const [loading, setLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const resposne = await fetch(url);
+    const newJobs = await resposne.json();
+    setJobs(newJobs);
+    setLoading(false)
+  }
+
+  const chagneJob = item => {
+    setValue(item)
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>Loading...</h1>
+      </section>
+    )
+  }
+
+  const { company, dates, duties, title } = jobs[value]
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>Experience</h2>
+        <div className="underline" />
+      </div>
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, index) => {
+            return (
+              <button className={`job-btn ${index === value && 'active-btn'}`} type="button" key={item.id} onClick={() => chagneJob(index)}>
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{ title }</h3>
+          <h4>{ company }</h4>
+          <p className="job-date">{ dates }</p>
+          {duties.map((duty, index) => {
+            return (
+              <div className='job-desc'>
+                <FaAngleDoubleRight className='job-icon' />
+                <p>{ duty }</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default App
